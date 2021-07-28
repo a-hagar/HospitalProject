efeceb7ba870b8f17a5f2785ecc26a72e3bc69d8
E-Wickham <ericwickham@hotmail.com>
2021-07-28 01:41:25 -0400

    Eric's adds to the DB

---
+++ HospitalProject/Models/Donation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HospitalProject.Models
+{
+    public class Donation
+    {
+        [Key]
+        public int DonationId { get; set; }
+
+        [ForeignKey("Donor")]
+        public int DonorId { get; set; }
+        public virtual Donor Donor { get; set; }
+
+        public DateTime DonationDate { get; set; }
+
+        public int Amount { get; set; }
+
+    }
+}
---
+++ HospitalProject/Models/Donor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HospitalProject.Models
+{
+    public class Donor
+    {
+        [Key]
+        public int DonorId { get; set; }
+
+        [ForeignKey("User")]
+        public int userid { get; set; }
+        public virtual User User { get; set; }
+
+
+    }
+}
+
+
